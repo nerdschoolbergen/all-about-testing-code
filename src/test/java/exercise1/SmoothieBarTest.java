db452405a6b5046b5aef5b824d5edf9f3b3a9068
Sindre Kjær <code@sindrekjr.dev>
2024-01-29 18:29:45 +0100

    Add helloNerdschool test

--- src/test/java/exercise1/SmoothieBarTest.java
+++ src/test/java/exercise1/SmoothieBarTest.java
@@ -5,11 +5,5 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class SmoothieBarTest {
     @Test
-    public void canBlendOrangeAndAppleSmoothie() {
-        SmoothieBar smoothieBar = new SmoothieBar();
-        smoothieBar.restockApples(2);
-        smoothieBar.restockOranges(2);
-        Smoothie smoothie = smoothieBar.blend(SmoothieKind.OrangeAndAppleSmoothie);
-        assertEquals(SmoothieKind.OrangeAndAppleSmoothie, smoothie.getKind());
-    }
+    public void helloNerdschool() { System.out.println("Hello Nerdschool"); }
 }
